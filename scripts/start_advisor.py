4eb98f2e5930b62a66d8fe15c90e01ded933888e
Ngin Yun Chuan <yunchuann@gmail.com>
2018-09-20 16:26:35 +0800

    Set Admin & Query Frontend to multi-thread

---
+++ scripts/start_advisor.py
@@ -0,0 +1,10 @@
+import os
+
+from rafiki.utils.log import configure_logging
+from rafiki.advisor import app
+
+configure_logging('advisor')
+
+if __name__ == "__main__":
+    # No threading since data is in-memory
+    app.run(host='0.0.0.0', port=os.getenv('ADVISOR_PORT', 8001), debug=True)
