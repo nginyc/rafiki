8a91a6a3fc084b22244ab04a1f7fe93a352fce68
Charlton Lim <cadmusthefounder@gmail.com>
2018-10-04 11:34:03 +0800

    Add more to example client

--- examples/client.py
+++ examples/client.py
@@ -1,18 +1,17 @@
 from rafiki.client import Client
 from rafiki.constants import DatasetTask
 
-def main():
-    client = Client()
-    client.login(email='superadmin@rafiki', password='rafiki')
-
-    client.create_model(
+def create_model(client):
+    model_data = client.create_model(
         name='multi_layer_perceptron',
         task=DatasetTask.IMAGE_CLASSIFICATION,
         model_file_path='examples/models/multi_layer_perceptron.py',
         model_class='MultiLayerPerceptron'
     )
+    print(model_data)
 
-    client.create_train_job(
+def create_train_job(client):
+    train_job_data = client.create_train_job(
         app='fashion_mnist_app',
         task=DatasetTask.IMAGE_CLASSIFICATION,
         train_dataset_uri='https://github.com/cadmusthefounder/mnist_data/blob/master/output/fashion_train.zip?raw=true',
@@ -20,6 +19,19 @@ def main():
         budget_type='MODEL_TRIAL_COUNT',
         budget_amount=3
     )
+    print(train_job_data)
+
+def get_best_trials_of_train_job(client):
+    best_trials = client.get_best_trials_of_train_job(app='fashion_mnist_app')
+    print(best_trials)
+
+def main():
+    client = Client()
+    client.login(email='superadmin@rafiki', password='rafiki')
+
+    # create_model(client)
+    # create_train_job(client)
+    get_best_trials_of_train_job(client)
 
 if __name__ == '__main__':
     main()
