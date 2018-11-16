22b4ba0b44dffec0f83e78080b45aab49f5c7d4a
Ngin Yun Chuan <yunchuann@gmail.com>
2018-11-16 16:50:45 +0800

    Remove debug mode from Flask apps

--- scripts/start_admin.py
+++ scripts/start_admin.py
@@ -15,5 +15,4 @@ if __name__ == "__main__":
     app.run(
         host='0.0.0.0', 
         port=os.getenv('ADMIN_PORT', 8000), 
-        debug=True,
         threaded=True)
--- scripts/start_predictor.py
+++ scripts/start_predictor.py
@@ -7,7 +7,6 @@ from rafiki.predictor.app import app
 def start_service(service_id, service_type):
     app.run(host='0.0.0.0', 
             port=os.getenv('PREDICTOR_PORT', 8002), 
-            debug=True, 
             threaded=True)
 
 def end_service(service_id, service_type):
