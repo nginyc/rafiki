41f9900db0729888964a3372957c892fa0335ab1
Ngin Yun Chuan <yunchuann@gmail.com>
2019-03-30 23:46:51 +0800

    Fix default user email in scripts

--- examples/scripts/image_classification/run_enas.py
+++ examples/scripts/image_classification/run_enas.py
@@ -4,6 +4,7 @@ import pprint
 
 from rafiki.client import Client
 from rafiki.model import serialize_knob_config
+from rafiki.config import SUPERADMIN_EMAIL
 from rafiki.constants import BudgetType, TaskType, ModelDependency
 from examples.models.image_classification.TfEnas import TfEnasTrain
 
@@ -97,7 +98,7 @@ if __name__ == '__main__':
     parser.add_argument('--enable_gpu', action='store_true', help='Whether to use GPU')
     parser.add_argument('--host', type=str, default=os.environ.get('RAFIKI_ADDR'), help='Host of Rafiki instance')
     parser.add_argument('--admin_port', type=int, default=os.environ.get('ADMIN_EXT_PORT'), help='Port for Rafiki Admin on host')
-    parser.add_argument('--email', type=str, default=os.environ.get('SUPERADMIN_EMAIL'), help='Email of user')
+    parser.add_argument('--email', type=str, default=SUPERADMIN_EMAIL, help='Email of user')
     parser.add_argument('--password', type=str, default=os.environ.get('SUPERADMIN_PASSWORD'), help='Password of user')
     (args, _) = parser.parse_known_args()
 
