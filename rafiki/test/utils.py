71182eb50e6156e6e0d08e218f6425224f10fba3
Ngin Yun Chuan <yunchuann@gmail.com>
2019-04-15 22:47:25 +0800

    Update tests for params store

--- rafiki/test/utils.py
+++ rafiki/test/utils.py
@@ -1,4 +1,5 @@
 import pytest
+from unittest.mock import MagicMock
 import numpy
 import random as rand
 
@@ -9,3 +10,36 @@ def global_setup():
     '''
     rand.seed(0)
     numpy.random.seed(0)
+
+@pytest.fixture(scope='session', autouse=True)
+def mock_redis():
+    import redis
+    redis.ConnectionPool.from_url = MagicMock() 
+    redis.StrictRedis = RedisMock
+
+class RedisMock():
+    data = {}
+    
+    def __init__(self, **kwargs):
+        pass
+
+    def get(self, key):
+        value = self.data.get(key)
+        if isinstance(value, str):
+            return value.encode()
+        
+        return value
+    
+    def set(self, key, value, **kwargs):
+        is_set = (key in self.data)
+        self.data[key] = value
+        return not is_set
+
+    def keys(self, patt):
+        # TODO: Do more accuracte implementation based on pattern
+        return self.data.keys()
+    
+    def delete(self, *keys):
+        for key in keys:
+            del self.data[key]
+
