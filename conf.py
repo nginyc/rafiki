c17ac26dde3c7e28e3df6c58766ec1f7c5e97e9e
Ngin Yun Chuan <yunchuann@gmail.com>
2019-05-06 10:50:29 +0800

    Update year

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('.'))
 # -- Project information -----------------------------------------------------
 
 project = 'Rafiki'
-copyright = '2018, nginyc, cadmusthefounder, nudles'
+copyright = '2019, nginyc, cadmusthefounder, nudles'
 author = 'nginyc, cadmusthefounder, nudles'
 
 # The short X.Y version
