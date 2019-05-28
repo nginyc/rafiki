770c899a529ceffd836b8fd684941b335cc40bdb
Ngin Yun Chuan <yunchuann@gmail.com>
2019-05-28 17:04:58 +0800

    Print test results to log

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,8 @@
+LOG_FILE_PATH=$PWD/logs/test.log
+
 # Read from shell configuration file
 source ./.env.sh
 
 pip install -r rafiki/test/requirements.txt
-pytest -s --cov=rafiki $1
+pytest -s --cov=rafiki $1 > $LOG_FILE_PATH || echo "Tests failed."
+echo "Test results at $LOG_FILE_PATH"
