88ce95362cce50022fdb4ee7e0f9f341ba0cf34f
Ngin Yun Chuan <yunchuann@gmail.com>
2019-06-08 11:05:17 +0800

    Update version

--- .env.sh
+++ .env.sh
@@ -1,7 +1,7 @@
 # Core external configuration for Rafiki
 export DOCKER_NETWORK=rafiki
 export DOCKER_SWARM_ADVERTISE_ADDR=127.0.0.1
-export RAFIKI_VERSION=0.0.9
+export RAFIKI_VERSION=0.2.0
 export RAFIKI_ADDR=127.0.0.1
 export ADMIN_EXT_PORT=3000
 export ADMIN_WEB_EXT_PORT=3001
