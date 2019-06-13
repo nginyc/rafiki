66448575ac6e6b1c9729e6b6f25ce5c5b00d23d3
Ngin Yun Chuan <yunchuann@gmail.com>
2019-06-13 09:01:45 -0700

    Confirm purging of metadata & data folders

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,7 +1,9 @@
+source ./scripts/utils.sh
+
 # Delete database dump
-rm $POSTGRES_DUMP_FILE_PATH
+delete_path "$POSTGRES_DUMP_FILE_PATH"
 
 # Clean all files within data, logs and params folder
-rm -rf $DATA_WORKDIR_PATH/*
-rm -rf $PARAMS_WORKDIR_PATH/*
-rm -rf $LOGS_WORKDIR_PATH/*
+delete_path "$DATA_WORKDIR_PATH/*"
+delete_path "$PARAMS_WORKDIR_PATH/*"
+delete_path "$LOGS_WORKDIR_PATH/*"
