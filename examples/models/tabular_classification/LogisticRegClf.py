17576c16a01650284a168b1e05cd445dd1733400
pinpom <thao.nguyen038@gmail.com>
2020-03-20 11:02:16 +0800

    change diabetes dataset processing method

--- examples/models/tabular_classification/LogisticRegClf.py
+++ examples/models/tabular_classification/LogisticRegClf.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pandas as pd
+import json 
 import pickle
-import base64  
+import base64
 import warnings
 warnings.filterwarnings("ignore")
-
 from sklearn.linear_model import LogisticRegression
 from sklearn.decomposition import PCA
 
@@ -31,42 +31,47 @@ class LogisticRegClf(BaseModel):
         self._clf = self._build_classifier(self.penalty, self.tol, self.C, self.fit_intercept, self.solver)
 
        
-    def train(self, dataset_path, **kwargs):       
+    def train(self, dataset_path, features=None, target=None, **kwargs):
+        # Record features & target
+        self._features = features
+        self._target = target
+        
         # Load CSV file as pandas dataframe
         csv_path = dataset_path
-        df = pd.read_csv(csv_path)
-        data = self.median_dataset(df)
-        
-        X_train = self.prepare_X(data)
-        y_train = data.iloc[:, -1]
+        data = pd.read_csv(csv_path)
 
-        self._clf.fit(X_train, y_train)
+        # Extract X & y from dataframe
+        (X, y) = self._extract_xy(data)
+
+        X = self.prepare_X(X)
+        
+        self._clf.fit(X, y)
 
         # Compute train accuracy
-        score = self._clf.score(X_train, y_train)
+        score = self._clf.score(X, y)
         logger.log('Train accuracy: {}'.format(score))
         
 
     def evaluate(self, dataset_path):
         # Load CSV file as pandas dataframe
         csv_path = dataset_path
-        df = pd.read_csv(csv_path)
-        data = self.median_dataset(df)
+        data = pd.read_csv(csv_path)
 
-        X_val = self.prepare_X(data)
-        y_val = data.iloc[:, -1]
-        
-        # Compute test accuracy
-        accuracy = self._clf.score(X_val, y_val)
+        # Extract X & y from dataframe
+        (X, y) = self._extract_xy(data)
+
+        X = self.prepare_X(X)
+
+        accuracy = self._clf.score(X, y)
         return accuracy
-
+    
     
     def predict(self, queries):
         queries = [pd.DataFrame(query, index=[0]) for query in queries]
-        data = self.median_dataset(queries)
+        data = self.prepare_X(queries)
         probs = self._clf.predict_proba(data)
         return probs.tolist()
-
+    
     
     def destroy(self):
         pass
@@ -79,38 +84,51 @@ class LogisticRegClf(BaseModel):
         clf_bytes = pickle.dumps(self._clf)
         clf_base64 = base64.b64encode(clf_bytes).decode('utf-8')
         params['clf_base64'] = clf_base64
+        params['features'] = json.dumps(self._features)
+        params['target'] = self._target
 
         return params
-    
 
     def load_parameters(self, params):
         # Load model parameters
         assert 'clf_base64' in params
         clf_base64 = params['clf_base64']
         clf_bytes = base64.b64decode(clf_base64.encode('utf-8'))
+
         self._clf = pickle.loads(clf_bytes)
+        self._features = json.loads(params['features'])
+        self._target = params['target']
+        
+        
+    def _extract_xy(self, data):
+        features = self._features
+        target = self._target
+
+        if features is None:
+            X = data.iloc[:,:-1]
+        else:
+            X = data[features]
+            
+        if target is None:
+            y = data.iloc[:,-1]
+        else:
+            y = data[target]
+
+        return (X, y)
         
         
     def median_dataset(self, df):
         #replace zero values by median so that 0 will not affect median.
-        df.Glucose.replace(0, np.nan, inplace=True)
-        df.Glucose.replace(np.nan, df['Glucose'].median(), inplace=True)
-        df.BloodPressure.replace(0, np.nan, inplace=True)
-        df.BloodPressure.replace(np.nan, df['BloodPressure'].median(), inplace=True)
-        df.SkinThickness.replace(0, np.nan, inplace=True)
-        df.SkinThickness.replace(np.nan, df['SkinThickness'].median(), inplace=True)
-        df.Insulin.replace(0, np.nan, inplace=True)
-        df.Insulin.replace(np.nan, df['Insulin'].median(), inplace=True)
-        df.BMI.replace(0, np.nan, inplace=True)
-        df.BMI.replace(np.nan, df['BMI'].median(), inplace=True)
+        for col in df.columns:
+            df[col].replace(0, np.nan, inplace=True)
+            df[col].fillna(df[col].median(), inplace=True)
         return df
 
     
-    def prepare_X(self, data):
-        X = data.iloc[:, :-1]
-        #Reduce data dimensionality
-        pca = PCA()
-        return pca.fit_transform(X)
+    def prepare_X(self, df):
+        data = self.median_dataset(df)
+        X = PCA().fit_transform(data)
+        return X
 
 
     def _build_classifier(self, penalty, tol, C, fit_intercept, solver):
@@ -131,8 +149,8 @@ if __name__ == '__main__':
         dependencies={
             ModelDependency.SCIKIT_LEARN: '0.20.0'
         },
-        train_dataset_path='data/diabetes.csv',
-        val_dataset_path='data/diabetes.csv',
+        train_dataset_path='data/diabetes_train.csv',
+        val_dataset_path='data/diabetes_val.csv',
         queries=[
             { 'Pregnancies': 3, 'Glucose': '130', 'BloodPressure': 92, 'SkinThickness': 30, 'Insulin': 90, 'BMI': 30.4, 'Age': 40 }
         ]
